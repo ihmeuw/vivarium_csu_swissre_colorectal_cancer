4b55bc90eacc9933ddbc38aabb2d3628cc7504e0
Abraham Flaxman <abie@uw.edu>
2021-01-05 19:26:01 -0800

    BUG: missed an age_group->age_cohort

--- src/vivarium_csu_swissre_colorectal_cancer/constants/results.py
+++ src/vivarium_csu_swissre_colorectal_cancer/constants/results.py
@@ -24,7 +24,7 @@ STANDARD_COLUMNS = {
 THROWAWAY_COLUMNS = [f'{state}_event_count' for state in models.STATES]
 
 TOTAL_POPULATION_COLUMN_TEMPLATE = 'total_population_{POP_STATE}'
-PERSON_TIME_COLUMN_TEMPLATE = 'person_time_in_{YEAR}_among_{SEX}_in_age_group_{AGE_GROUP}'
+PERSON_TIME_COLUMN_TEMPLATE = 'person_time_in_{YEAR}_among_{SEX}_in_age_cohort_{AGE_COHORT}'
 DEATH_COLUMN_TEMPLATE = 'death_due_to_{CAUSE_OF_DEATH}_in_{YEAR}_among_{SEX}_in_age_cohort_{AGE_COHORT}'
 YLLS_COLUMN_TEMPLATE = 'ylls_due_to_{CAUSE_OF_DEATH}_in_{YEAR}_among_{SEX}_in_age_cohort_{AGE_COHORT}'
 YLDS_COLUMN_TEMPLATE = 'ylds_due_to_{CAUSE_OF_DISABILITY}_in_{YEAR}_among_{SEX}_in_age_cohort_{AGE_COHORT}'
